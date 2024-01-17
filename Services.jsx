2a005c067f3058a891b9062cf3a8855611c4dc91
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-17 12:26:18 +0545

    Create Services.jsx

---
+++ Services.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Cards from "./Cards";
+import SData from "./SData";
+
+const Services = () => {
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center Services">Our Services</h1>
+      </div>
+      <div className="container-fluid-mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row gy-4">
+              {SData.map((val, ind) => (
+                <Cards key={ind} imgsrc={val.imgsrc} title={val.title} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
