fc059acc4536a9661d8cb78087c8359f6b6b8e2e
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-17 12:25:57 +0545

    Create SData.jsx

---
+++ SData.jsx
@@ -0,0 +1,32 @@
+ import "./logo.hotel.png"
+ import "./web.jpg"
+
+const SData=[
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"Web Development"
+    },
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"App Development"
+    },
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"Software Development"
+    },
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"Digital Marketing"
+    },
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"Android Development"
+    },
+    {
+        imgsrc:require("./logo.hotel.png").default,
+        title:"Marketing"
+    }
+    
+]
+
+export default SData;
