07993cf0061cb189bbf782af1ea9fe8abcc170b5
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-17 12:25:13 +0545

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer=()=>{
+    return(
+        <>
+  <div className="footer text-center " style={{position: "absolute", bottom: 0, width: "100%"}}>
+    © 2024 Copyright : I-yush.
+  </div>
+        </>
+    )
+}
+
+export default Footer;
