bffd5472f6f76b602f04949c7ec7b4ab34035172
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-18 08:19:18 +0545

    Update SData.jsx

--- SData.jsx
+++ SData.jsx
@@ -1,32 +1,22 @@
- import "./logo.hotel.png"
- import "./web.jpg"
-
 const SData=[
     {
-        imgsrc:require("./logo.hotel.png").default,
-        title:"Web Development"
-    },
-    {
-        imgsrc:require("./logo.hotel.png").default,
         title:"App Development"
     },
     {
-        imgsrc:require("./logo.hotel.png").default,
-        title:"Software Development"
+        title:"Web Development"
     },
     {
-        imgsrc:require("./logo.hotel.png").default,
         title:"Digital Marketing"
     },
     {
-        imgsrc:require("./logo.hotel.png").default,
         title:"Android Development"
     },
     {
-        imgsrc:require("./logo.hotel.png").default,
-        title:"Marketing"
-    }
-    
+        title:"Python"
+    },
+    {
+        title:"Mern Stack"
+    },
 ]
 
 export default SData;
